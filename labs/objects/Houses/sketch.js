c129545d902052a579e8a11f7545abe55590f538
John Dempsey <john.dempsey@iadt.ie>
2025-11-09 18:40:28 +0000

    Adding labs/objects/Houses

---
+++ labs/objects/Houses/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(800, 450);
+  noLoop();
+  noStroke();
+  print("Hello World!");
+}
+
+function draw() {
+  // sky
+  background(135, 206, 236);
+  // ground
+  fill(124, 252, 0);
+  rect(0, height * 0.75, width, height * 0.25);
+  // sun
+  fill(255, 255, 0);
+  ellipse(700, 80, 80, 80);
+
+  let house1 = new House(100, 250, 100, 100, 3);
+  let house2 = new House(250, 250, 200, 150, 5);
+  let house3 = new House(500, 250, 150, 180, 2);
+
+  house1.draw();
+  house2.draw();
+  house3.draw();
+}
